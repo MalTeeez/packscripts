49a45d0c4c9ad2d64d0a76c292bfb665836a56b2
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2025-11-08 15:24:56 +0100

    Catch trailing slashes on config provided paths

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,6 +3,6 @@ if (await Bun.file("./config.json").exists()) {
     config = await Bun.file("./config.json").json();
 }
 
-export const MOD_BASE_DIR = config?.MOD_BASE_DIR || "../.minecraft/mods"
-export const DOWNLOAD_TEMP_DIR = config?.DOWNLOAD_TEMP_DIR || './tmp/downloads';
-export const ANNOTATED_FILE = config?.ANNOTATED_FILE || '../annotated_mods.json';
+export const MOD_BASE_DIR = config?.MOD_BASE_DIR?.replace(/\/$/m, "") || "../.minecraft/mods"
+export const DOWNLOAD_TEMP_DIR = config?.DOWNLOAD_TEMP_DIR?.replace(/\/$/m, "") || './tmp/downloads';
+export const ANNOTATED_FILE = config?.ANNOTATED_FILE?.replace(/\/$/m, "") || '../annotated_mods.json';
